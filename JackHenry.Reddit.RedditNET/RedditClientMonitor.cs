f771ce244dfaef2bfe0bd510e54532066b31645c
Chris Sutherland <cStorm128@gmail.com>
2023-10-13 16:21:34 -0500

    include "secret" parameter in README.md

---
+++ JackHenry.Reddit.RedditNET/RedditClientMonitor.cs
@@ -0,0 +1,115 @@
+﻿using Reddit;
+using Reddit.Controllers;
+
+namespace JackHenry.Reddit.RedditNET;
+
+public class RedditClientMonitor : IRedditMonitor
+{
+    private readonly RedditClient _client;
+    private readonly IRedditReader _redditReader;
+
+    private Subreddit? _sub;
+    private Thread? _updateThread;
+
+    private readonly Dictionary<string, PostSummary> _posts = new();
+    private DateTime? _oldest;
+
+    public RedditClientMonitor(ApiCredentials credentials, IRedditReader redditReader)
+    {
+        _client = new(credentials.AppId, credentials.RefreshToken, credentials.AppSecret);
+        _redditReader = redditReader ?? throw new ArgumentNullException(nameof(redditReader));
+    }
+
+    public event EventHandler<PostsEventArgs>? PostsAdded;
+    public event EventHandler<PostsEventArgs>? PostsUpdated;
+
+    private static PostSummary CreatePostSummary(Post post) => new(post.Author, post.Title, post.Id, post.UpVotes);
+
+    public void Start(string subName)
+    {
+        if (_sub != null) throw new InvalidOperationException("Already running");
+
+        _sub = _client.Subreddit(subName).About();
+        _sub.Posts.NewUpdated += (sender, e) =>
+        {
+            var added = e.Added.Select(AddPost).ToArray();
+            PostsAdded?.Invoke(this, new PostsEventArgs(added));
+        };
+        if (!_sub.Posts.MonitorNew())
+            throw new InvalidOperationException("Expected monitored");
+
+        MonitorPosts();
+    }
+
+    private PostSummary AddPost(Post post)
+    {
+        PostSummary summary = CreatePostSummary(post);
+        _posts.Add(summary.Id, summary);
+        if (_oldest > post.Created || _oldest == null)
+            _oldest = post.Created;
+        return summary;
+    }
+
+    private void MonitorPosts()
+    {
+        _updateThread = new Thread(() =>
+        {
+            while (_updateThread != null)
+                try
+                {
+                    CheckPosts();
+                }
+                catch { }
+        });
+        _updateThread.Start();
+    }
+
+    private void CheckPosts()
+    {
+        string? subreddit = _sub?.Name;
+        if (subreddit != null)
+        {
+            IIncrementalReader<PostSummary> incremental = _redditReader.GetLatestPosts(subreddit, _oldest);
+            while (!incremental.Done)
+            {
+                List<PostSummary> changed = incremental.Enumerate().Where(UpdatePost).ToList();
+                if (changed.Count != 0)
+                    PostsUpdated?.Invoke(this, new PostsEventArgs(changed));
+            }
+        }
+    }
+
+    private bool UpdatePost(PostSummary latest)
+    {
+        if (_posts.TryGetValue(latest.Id, out PostSummary? summary))
+        {
+            if (!summary.Equals(latest))
+            {
+                _posts[latest.Id] = latest ?? throw new ArgumentNullException(nameof(latest));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void Stop()
+    {
+        if (_sub == null) throw new InvalidOperationException("Not running");
+
+        (Subreddit sub, _sub) = (_sub, null);
+        (Thread? thread, _updateThread) = (_updateThread, null);
+        bool monitored = sub.Posts.MonitorNew();
+        thread?.Join();
+        _posts.Clear();
+        _oldest = null;
+
+        if (monitored)
+            throw new InvalidOperationException("Expected unmonitored");
+    }
+
+    public void Dispose()
+    {
+        if (_sub != null)
+            Stop();
+    }
+}
--- README.md
+++ README.md
@@ -6,12 +6,12 @@ Create an OAuth app here: https://www.reddit.com/prefs/apps
 
 Get API refresh_token:
 ```shell
-> dotnet run  auth <api-app-id> --port=8080
+> dotnet run  auth <api-app-id> [<api-app-secret>] [--port=8080]
 ```
 
 Monitor specific subreddit:
 ```shell
-> dotnet run  watch AskReddit <api-app-id> --refresh <api-refresh-token>
+> dotnet run  watch AskReddit <api-app-id> [<api-app-secret>] --refresh <api-refresh-token>
 ```
 
 ## Programming Assignment (Requirements)
@@ -21,10 +21,10 @@ Your app should consume the posts from your chosen subreddit in near real time a
 -   Posts with most up votes
 -   Users with most posts
 
-Your app should also provide some way to report these values to a user (periodically log to terminal, return from RESTful web service, etc.). If there are other interesting statistics youÅfd like to collect, that would be great. There is no need to store this data in a database; keeping everything in-memory is fine. That said, you should think about how you would persist data if that was a requirement.
+Your app should also provide some way to report these values to a user (periodically log to terminal, return from RESTful web service, etc.). If there are other interesting statistics you'd like to collect, that would be great. There is no need to store this data in a database; keeping everything in-memory is fine. That said, you should think about how you would persist data if that was a requirement.
 
 To acquire near real time statistics from Reddit, you will need to continuously request data from Reddit's rest APIs. Reddit implements rate limiting and provides details regarding rate limit used, rate limit remaining, and rate limit reset period via response headers. Your application should use these values to control throughput in an even and consistent manner while utilizing a high percentage of the available request rate.
 
-ItÅfs very important that the various application processes do not block each other as Reddit can have a high volume on many of their subreddits. The app should process posts as concurrently as possible to take advantage of available computing resources. While we are only asking to track a single subreddit, you should be thinking about his you could scale up your app to handle multiple subreddits.
+It's very important that the various application processes do not block each other as Reddit can have a high volume on many of their subreddits. The app should process posts as concurrently as possible to take advantage of available computing resources. While we are only asking to track a single subreddit, you should be thinking about his you could scale up your app to handle multiple subreddits.
 
 While designing and developing this application, you should keep SOLID principles in mind. Although this is a code challenge, we are looking for patterns that could scale and are loosely coupled to external systems / dependencies. In that same theme, there should be some level of error handling and unit testing. The submission should contain code that you would consider production ready.
